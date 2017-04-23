c772a336bb47ba9f35ab93cecdc33c47bda10003
Andrew Boychuk <andrewboychuk@gmail.com>
2017-04-24 00:18:51 +0300

    C

--- Objective-C/main.m
+++ Objective-C/main.m
@@ -6,24 +6,10 @@
 //  Copyright © 2017 Andrew Boychuk. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
-#import "ABCreature.h"
+#import "ABObjCSyntax.h"
 
 int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        ABCreature *creature1 = [[ABCreature new] autorelease];
-        ABCreature *creature2 = [[ABCreature new] autorelease];
-        ABCreature *creature3 = [[ABCreature new] autorelease];
-        ABCreature *creature4 = [[ABCreature new] autorelease];
-        ABCreature *child1 = [[ABCreature new] autorelease];
-        
-        
-        creature1.age = 23;
-        creature1.name = @"Creature1";
-        creature1 = Male;
-        [creature1 sayHello];
-    
-        NSArray *creatureArray = [NSArray arrayWithObjects:creature1, creature2, creature3, creature4, nil];
-    }
+    void ABObjCSyntax();
+
     return 0;
 }
