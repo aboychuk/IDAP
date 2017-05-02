a55fb158808d7a123adbcd2641634e796a5100df
Andrew Boychuk <andrewboychuk@gmail.com>
2017-05-02 21:05:12 +0300

    randomNumber method added

---
+++ Objective-C/Source/ABCategory/NSNumber+ABRandomNumber.m
@@ -0,0 +1,13 @@
+//
+//  NSNumber+ABRandomNumber.m
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 5/2/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "NSNumber+ABRandomNumber.h"
+
+@implementation NSNumber (ABRandomNumber)
+
+@end
