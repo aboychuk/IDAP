4dcea9606c3cc46aef6be613e0c47c1aaa4be31f
Andrew Boychuk <andrewboychuk@gmail.com>
2017-05-04 22:02:47 +0300

    ABCarWash project file tree restructured

---
+++ Objective-C/Source/ABCarWash/ABCarWash.h
@@ -0,0 +1,13 @@
+//
+//  ABCarWash.h
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 5/4/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABCarWash : NSObject
+
+@end
---
+++ Objective-C/Source/ABCarWash/ABCarWash.m
@@ -0,0 +1,13 @@
+//
+//  ABCarWash.m
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 5/4/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABCarWash.h"
+
+@implementation ABCarWash
+
+@end
