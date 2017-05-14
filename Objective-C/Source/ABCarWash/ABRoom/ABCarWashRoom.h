0969650ae5324da168cc66a9b2163b3f134a6bd2
Andrew Boychuk <andrewboychuk@gmail.com>
2017-05-14 20:38:20 +0300

    class added -> ABCarWashRoom

---
+++ Objective-C/Source/ABCarWash/ABRoom/ABCarWashRoom.h
@@ -0,0 +1,13 @@
+//
+//  ABCarWashRoom.h
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 5/14/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABRoom.h"
+
+@interface ABCarWashRoom : ABRoom
+
+@end
---
+++ Objective-C/Source/ABCarWash/ABRoom/ABCarWashRoom.m
@@ -0,0 +1,13 @@
+//
+//  ABCarWashRoom.m
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 5/14/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABCarWashRoom.h"
+
+@implementation ABCarWashRoom
+
+@end
