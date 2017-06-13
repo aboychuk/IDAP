f7c2c21709c8c8af98d8fbf7f4de3b77f4fb3e80
Andrew Boychuk <andrewboychuk@gmail.com>
2017-06-13 16:26:59 +0300

    ABThreadUnsafeObject added.

---
+++ Objective-C/Source/Syntax/Classes/ABThreading/ABThreadUnsafeObject.h
@@ -0,0 +1,14 @@
+//
+//  ABThreadUnsafeObject.h
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 6/13/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABThreadUnsafeObject : NSObject
+@property (nonatomic, assign)   NSUInteger  value;
+
+@end
---
+++ Objective-C/Source/Syntax/Classes/ABThreading/ABThreadUnsafeObject.m
@@ -0,0 +1,13 @@
+//
+//  ABThreadUnsafeObject.m
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 6/13/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABThreadUnsafeObject.h"
+
+@implementation ABThreadUnsafeObject
+
+@end
