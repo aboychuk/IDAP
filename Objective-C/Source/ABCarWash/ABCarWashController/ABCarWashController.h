391c195a3b29cff6aaf1e34deb83d94a3792ad84
Andrew Boychuk <andrewboychuk@gmail.com>
2017-06-21 15:41:43 +0300

    ABCarWashController -> class added.

---
+++ Objective-C/Source/ABCarWash/ABCarWashController/ABCarWashController.h
@@ -0,0 +1,13 @@
+//
+//  ABCarWashController.h
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 6/21/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABCarWashController : NSObject
+
+@end
---
+++ Objective-C/Source/ABCarWash/ABCarWashController/ABCarWashController.m
@@ -0,0 +1,13 @@
+//
+//  ABCarWashController.m
+//  IDAPCourse
+//
+//  Created by Andrew Boychuk on 6/21/17.
+//  Copyright © 2017 Andrew Boychuk. All rights reserved.
+//
+
+#import "ABCarWashController.h"
+
+@implementation ABCarWashController
+
+@end
