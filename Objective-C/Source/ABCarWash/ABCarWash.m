69b58305e45b12d01ca9ca53993addedc0f5260b
Andrew Boychuk <andrewboychuk@gmail.com>
2017-06-27 15:23:01 +0300

    ABCarWash -> refactored.

--- Objective-C/Source/ABCarWash/ABCarWash.m
+++ Objective-C/Source/ABCarWash/ABCarWash.m
@@ -13,9 +13,6 @@
 static NSUInteger ABWashersCountMax = 9;
 
 @interface ABCarWash ()
-@property (nonatomic, retain)   ABAcountant         *accountant;
-@property (nonatomic, retain)   ABDirector          *director;
-@property (nonatomic, retain)   NSArray             *washers;
 @property (nonatomic, retain)   ABCarWashController *controller;
 
 
@@ -27,11 +24,6 @@ static NSUInteger ABWashersCountMax = 9;
 #pragma mark Initializations and Deallocations
 
 - (void)dealloc {
-    [self removeObservers];
-
-    self.accountant = nil;
-    self.director = nil;
-    self.washers = nil;
     self.controller = nil;
 
     [super dealloc];
@@ -39,11 +31,9 @@ static NSUInteger ABWashersCountMax = 9;
 
 - (instancetype)init {
     self = [super init];
-    
-    self.accountant = [ABAcountant object];
-    self.director = [ABDirector object];
+    if (self) {
     self.controller = [ABCarWashController object];
-    [self setCarWashHierarchy];
+    }
     
     return self;
 }
@@ -52,40 +42,7 @@ static NSUInteger ABWashersCountMax = 9;
 #pragma mark Public Methods
 
 - (void)washCars:(NSArray *)cars{
-    [self.controller washCars:];
-}
-
-#pragma mark
-#pragma mark Private Methods
-
-- (void)setCarWashHierarchy {
-    ABAcountant *accountant = self.accountant;
-    ABCarWashController *controller = self.controller;
-    
-    self.washers = [NSArray objectsWithCount:ABRandomWithMaxValue(ABWashersCountMax)
-                                factoryBlock:^id{
-                                    ABCarWasher *washer = [ABCarWasher object];
-                                    [washer addObserver:accountant];
-                                    [washer addObserver:controller];
-                                    
-                                    return washer;
-    }];
-    
-    [accountant addObserver:self.director];
-    [controller createWashersQueue:self.washers];
-
-
-}
-
-- (void)removeObservers {
-    ABAcountant *accountant = self.accountant;
-    
-    for (ABCarWasher *washer in self.washers) {
-        [washer removeObserver:accountant];
-        [washer removeObserver:self.controller];
-    }
-    
-    [accountant removeObserver:self.director];
+    [self.controller washCars:cars];
 }
 
 @end
